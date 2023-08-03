076f6e30de9b5ce78640453f037ab4dd296a41a2
Luke Orellana <36606066+lukeorellana@users.noreply.github.com>
2023-08-03 15:52:49 +0000

    Fix snapshot for Log Analytics construct

--- src/azure-loganalytics/test/AzureLogAnalytics.spec.ts
+++ src/azure-loganalytics/test/AzureLogAnalytics.spec.ts
@@ -2,41 +2,42 @@ import { Testing, TerraformStack} from 'cdktf';
 import { exampleAzureLogAnalytics} from './ExampleAzureLogAnalytics'
 import 'cdktf/lib/testing/adapters/jest';
 import { AzureLogAnalytics } from '../';
-import { RoleAssignment } from "@cdktf/provider-azurerm/lib/role-assignment";
-//import { log } from 'console';
+//import { RoleAssignment } from "@cdktf/provider-azurerm/lib/role-assignment";
+import {AzurermProvider} from "@cdktf/provider-azurerm/lib/provider";
 
-
-
-describe('Log Analytics Workspace Only Defaults', () => {
+describe('Log Analytics Workspace With Defaults', () => {
   let stack: TerraformStack;
-  
+  let fullSynthResult: any;
+
   beforeEach(() => {
     const app = Testing.app();
     stack = new TerraformStack(app, "test");
-  
-    Testing.synthScope((stack) => {
-      new AzureLogAnalytics(stack, 'testAzureLogAnalyticsDefaults', {
-        name: "la-test",
-        location: 'eastus',
-        resource_group_name: "rg-test",
-      });
+
+    new AzurermProvider(stack, "azureFeature", {features: {}});
+    new AzureLogAnalytics(stack, 'testAzureLogAnalyticsDefaults', {
+      name: "la-test",
+      location: 'eastus',
+      resource_group_name: "rg-test",
     });
+
+    fullSynthResult = Testing.fullSynth(stack); // Save the result for reuse
   });
 
-  it('renders the Example Log Analytics Workspace and checks snapshot', () => {
-    expect(stack).toMatchSnapshot();
+  it("renders a Log Analytics Workspace with Defaults and checks snapshot", () => {
+    expect(
+      Testing.synth(stack)
+    ).toMatchSnapshot(); // Compare the already prepared stack
   });
 
   it("check if the produced terraform configuration is valid", () => {
-    // We need to do a full synth to validate the terraform configuration
-    expect(Testing.fullSynth(stack)).toBeValidTerraform();
+    expect(fullSynthResult).toBeValidTerraform(); // Use the saved result
   });
 
   it("check if this can be planned", () => {
-    // We need to do a full synth to plan the terraform configuration
-    expect(Testing.fullSynth(stack)).toPlanSuccessfully();
+    expect(fullSynthResult).toPlanSuccessfully(); // Use the saved result
   });
 });
+
 
 describe('Log Analytics Workspace Example', () => {
   let stack: TerraformStack;
@@ -45,13 +46,16 @@ describe('Log Analytics Workspace Example', () => {
     const app = Testing.app();
     stack = new TerraformStack(app, "test");
   
-    Testing.synthScope(() => {
+    Testing.synthScope((stack) => {
       new exampleAzureLogAnalytics(stack, "testAzureLogAnalytics");
     });
   });
 
-  it('renders the Example Log Analytics Workspace and checks snapshot', () => {
-    expect(stack).toMatchSnapshot();
+  it("renders the Example Log Analytics Workspace and checks snapshot", () => {
+    expect(
+      Testing.synth(new exampleAzureLogAnalytics(Testing.app(), "testAzureLogAnalytics"))
+      
+    ).toMatchSnapshot();
   });
 
   it("check if the produced terraform configuration is valid", () => {
@@ -63,38 +67,4 @@ describe('Log Analytics Workspace Example', () => {
     // We need to do a full synth to plan the terraform configuration
     expect(Testing.fullSynth(stack)).toPlanSuccessfully();
   });
-
-  it("check for Log Analytics Contributor Role Assignment", () => {
-    const app = Testing.app();
-    const stack = new exampleAzureLogAnalytics(app, "testAzureLogAnalytics");
-    const synthesized = Testing.synth(stack);
-
-    expect(synthesized).toHaveResourceWithProperties(RoleAssignment, {
-      role_definition_name: "Log Analytics Contributor",
-    });
-  });
-
-  it("check for Log Analytics Reader Role Assignment", () => {
-    const app = Testing.app();
-    const stack = new exampleAzureLogAnalytics(app, "testAzureLogAnalytics");
-    const synthesized = Testing.synth(stack);
-
-    expect(synthesized).toHaveResourceWithProperties(RoleAssignment, {
-      role_definition_name: "Log Analytics Reader",
-    });
-  });
-
-  it("check for Custom Role Assignment", () => {
-    const app = Testing.app();
-    const stack = new exampleAzureLogAnalytics(app, "testAzureLogAnalytics");
-    const synthesized = Testing.synth(stack);
-
-    expect(synthesized).toHaveResourceWithProperties(RoleAssignment, {
-      role_definition_name: "Monitoring Reader",
-    });
-  });
 });
-
-
-
-
