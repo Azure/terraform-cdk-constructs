c22b9d9e859a8c136796ca76705ed8b8a86cbcd0
Luke Orellana <36606066+lukeorellana@users.noreply.github.com>
2023-08-07 13:54:41 +0000

    Reduce path repeats on e2e tests

--- src/azure-resourcegroup/test/ResourceGroup_test.go
+++ src/azure-resourcegroup/test/ResourceGroup_test.go
@@ -16,22 +16,27 @@ import (
 func TestTerraformCDKAzureResourceGroupExample(t *testing.T) {
 	t.Parallel()
 
+	stack_dir := "../../../cdktf.out/stacks/testAzureResourceGroup"
+	example_file := "./src/azure-resourcegroup/test/ExampleAzureResourceGroup.ts"
+
 	// subscriptionID is overridden by the environment variable "ARM_SUBSCRIPTION_ID"
 	subscriptionID := util.GetSubscriptionID()
 	os.Setenv("ARM_SUBSCRIPTION_ID", subscriptionID)
 
 	cmd := shell.Command{
 		Command:    "cdktf",
-		Args:       []string{"synth", "--app", "npx ts-node ./src/azure-resourcegroup/test/ExampleAzureResourceGroup.ts"},
+		Args:       []string{"synth", "--app", "npx ts-node" + " " + example_file},
 		WorkingDir: "../../../",
 	}
 
 	shell.RunCommandAndGetStdOut(t, cmd)
 
+	util.RandomizeUniqueResources(stack_dir + "/cdk.tf.json")
+
 	terraformOptions := &terraform.Options{
 
 		// The path to where our Terraform code is located
-		TerraformDir: "../../../cdktf.out/stacks/testAzureResourceGroup",
+		TerraformDir: stack_dir,
 	}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
