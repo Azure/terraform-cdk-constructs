b2db3d561159742c53360441d8d4e38be6b1d6d8
Luke Orellana <36606066+lukeorellana@users.noreply.github.com>
2023-11-07 21:25:46 +0000

    Add virtual machine construct and tests with documentation

---
+++ src/azure-virtualmachinescaleset/test/AzureWindowsVirtualMachineScaleSet.spec.ts
@@ -0,0 +1,64 @@
+import { Testing, TerraformStack} from 'cdktf';
+import 'cdktf/lib/testing/adapters/jest';
+import {AzureWindowsVirtualMachineScaleSet} from '..';
+import {AzurermProvider} from "@cdktf/provider-azurerm/lib/provider";
+import { exampleAzureWindowsVirtualMachineScaleSet } from '../test/ExampleAzureWindowsVirtualMachineScaleSet';
+
+describe('Azure Windows Virtual Machine Scale Set With Defaults', () => {
+  let stack: TerraformStack;
+  let fullSynthResult: any;
+
+  beforeEach(() => {
+    const app = Testing.app();
+    stack = new TerraformStack(app, "testAzureVMSSWithDefaults");
+
+    new AzurermProvider(stack, "azureFeature", {features: {}});
+
+    new AzureWindowsVirtualMachineScaleSet(stack, "testVirtualMachineScaleSet", {
+      resourceGroupName: "testResourceGroup",
+      adminUsername: "testAdmin",
+      adminPassword: "testPassword&@34$$123",
+      
+    });
+
+    fullSynthResult = Testing.fullSynth(stack); // Save the result for reuse
+  });
+
+  it("renders an Azure Windows Virtual Machine  Scale Set with defaults and checks snapshot", () => {
+    expect(
+      Testing.synth(stack)
+    ).toMatchSnapshot(); // Compare the already prepared stack
+  });
+
+  it("check if the produced terraform configuration is valid", () => {
+    expect(fullSynthResult).toBeValidTerraform(); // Use the saved result
+  });
+
+  it("check if this can be planned", () => {
+    expect(fullSynthResult).toPlanSuccessfully(); // Use the saved result
+  });
+});
+
+
+describe('Azure Windows Virtual Machine  Scale Set Example', () => {
+  
+  it("renders the Azure Windows Virtual Machine Scale Set and checks snapshot", () => {
+  
+    expect(
+      Testing.synth(new exampleAzureWindowsVirtualMachineScaleSet(Testing.app(), "testAzureWindowsVirtualMachineScaleSetExample"))
+      
+    ).toMatchSnapshot();
+});
+
+  it("check if the produced terraform configuration is valid", () => {
+    // We need to do a full synth to plan the terraform configuration
+    expect(Testing.fullSynth(new exampleAzureWindowsVirtualMachineScaleSet(Testing.app(), "testAzureWindowsVirtualMachineScaleSetExample"))).toBeValidTerraform();
+  });
+
+  it("check if this can be planned", () => {
+  
+    
+    // We need to do a full synth to plan the terraform configuration
+    expect(Testing.fullSynth(new exampleAzureWindowsVirtualMachineScaleSet(Testing.app(), "testAzureWindowsVirtualMachineScaleSetExample"))).toPlanSuccessfully();
+  });
+});
---
+++ src/azure-virtualmachinescaleset/test/ExampleAzureWindowsVirtualMachineScaleSet.ts
@@ -0,0 +1,126 @@
+import * as cdktf from "cdktf";
+import { AzureWindowsVirtualMachineScaleSet } from '..';
+import {VirtualNetwork} from "@cdktf/provider-azurerm/lib/virtual-network";
+import {Subnet} from "@cdktf/provider-azurerm/lib/subnet";
+import {BaseTestStack} from "../../testing";
+import { App} from "cdktf";
+import {ResourceGroup} from "@cdktf/provider-azurerm/lib/resource-group";
+import {AzurermProvider} from "@cdktf/provider-azurerm/lib/provider";
+import { Construct } from 'constructs';
+import { DataAzurermClientConfig } from "@cdktf/provider-azurerm/lib/data-azurerm-client-config";
+import { StorageAccount } from "@cdktf/provider-azurerm/lib/storage-account";
+
+
+const app = new App();
+
+export class exampleAzureWindowsVirtualMachineScaleSet extends BaseTestStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const clientConfig = new DataAzurermClientConfig(this, 'CurrentClientConfig', {});
+
+    new AzurermProvider(this, "azureFeature", {
+        features: {},
+      });
+
+    const resourceGroup = new ResourceGroup(this, "rg", {
+      location: 'eastus',
+      name: `rg-${this.name}`,
+
+    });
+
+    const vnet = new VirtualNetwork(this, "vnet", {
+      name: `vnet-${this.name}`,
+      location: resourceGroup.location,
+      resourceGroupName: resourceGroup.name,
+      addressSpace: ["10.0.0.0/16"],
+
+    });
+
+    const subnet = new Subnet(this, "subnet1", {
+      name: "subnet1",
+      resourceGroupName: resourceGroup.name,
+      virtualNetworkName: vnet.name,
+      addressPrefixes: ["10.0.1.0/24"],
+    });
+
+    const storage = new StorageAccount(this, "storage", {
+      name: `sta${this.name}`,
+      resourceGroupName: resourceGroup.name,
+      location: resourceGroup.location,
+      accountReplicationType: "LRS",
+      accountTier: "Standard",
+      minTlsVersion: "TLS1_2",
+      publicNetworkAccessEnabled: false,
+      networkRules: {
+        bypass: ['AzureServices'],
+        defaultAction: 'Deny',
+      },
+    });
+
+    
+
+    const vm = new AzureWindowsVirtualMachineScaleSet(this, 'vmss', {
+      name: this.name,
+      location: "eastus",
+      resourceGroupName: resourceGroup.name,
+      sku: "Standard_B1s",
+      adminUsername: "testadmin",
+      adminPassword: "Password1234!",
+      osDisk: {
+        caching: "ReadWrite",
+        storageAccountType: "Standard_LRS",
+      },
+      sourceImageReference: {
+        publisher: "MicrosoftWindowsServer",
+        offer: "WindowsServer",
+        sku: "2019-Datacenter",
+        version: "latest"
+      },
+      subnet: subnet,
+      publicIPAddress: [{
+        name: "testpublicip",
+      }],
+      boostrapCustomData: "Install-WindowsFeature -Name Web-Server; $website = '<h1>Hello World!</h1>'; Set-Content \"C:\\inetpub\\wwwroot\\iisstart.htm\" $website",
+      bootDiagnosticsStorageURI: storage.primaryBlobEndpoint,
+    });
+
+    // Diag Settings
+    vm.addDiagSettings({name: "diagsettings", storageAccountId: storage.id})
+
+    // RBAC
+    vm.addAccess(clientConfig.objectId, "Contributor")
+
+
+
+
+    // Outputs to use for End to End Test
+    const cdktfTerraformOutputRGName = new cdktf.TerraformOutput(this, "resource_group_name", {
+      value: resourceGroup.name,
+    });
+
+    const cdktfTerraformOutputNsgName = new cdktf.TerraformOutput(this, "vm_name", {
+      value: vm.name,
+    });
+
+    const cdktfTerraformOutputVmsize = new cdktf.TerraformOutput(this, "vm_size", {
+      value: "Standard_B1s",
+    });
+
+    const cdktfTerraformOutputVmEndpoint = new cdktf.TerraformOutput(this, "public_ip_name", {
+      value: "testpublicip",
+    });
+   
+
+    cdktfTerraformOutputRGName.overrideLogicalId("resource_group_name");
+    cdktfTerraformOutputNsgName.overrideLogicalId("vm_name");
+    cdktfTerraformOutputVmsize.overrideLogicalId("vm_size");
+    cdktfTerraformOutputVmEndpoint.overrideLogicalId("public_ip_name");
+
+  }
+}
+
+
+new exampleAzureWindowsVirtualMachineScaleSet(app, "testAzureWindowVirtualMachineScaleSetExample");
+
+app.synth();
