39a8b3442c4a748a5a3044bb146de8e68f12a5d8
Luke Orellana <36606066+lukeorellana@users.noreply.github.com>
2023-12-14 01:24:44 +0000

    Add src index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-// this is the entry point for the npm package
-// anything we want consumable (module, type, class, etc) should be exported here
-export * from './azure-applicationinsights';
-export * from './azure-loganalytics';
-export * from './azure-containerregistry';
-export * from './azure-resourcegroup';
-export * from './azure-keyvault';
+export * as azure_applicationinsights from './azure-applicationinsights';
+export * as azure_containerregistry from './azure-containerregistry';
+export * as azure_eventhub from './azure-eventhub';
+export * as azure_functionapp from './azure-functionapp';
+export * as azure_keyvault from './azure-keyvault';
+export * as azure_kusto from './azure-kusto';
+export * as azure_loganalytics from './azure-loganalytics';
+export * as azure_networksecuritygroup from './azure-networksecuritygroup';
+export * as azure_resourcegroup from './azure-resourcegroup';
+export * as azure_storageaccount from './azure-storageaccount';
+export * as azure_virtualmachine from './azure-virtualmachine';
+export * as azure_virtualmachinescaleset from './azure-virtualmachinescaleset';
+export * as azure_virtualnetwork from './azure-virtualnetwork';
+export * as core_azure from './core-azure';
+export * as testing from './testing';
+export * as util from './util';
