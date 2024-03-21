fae8984617fe12f054fc2addf8296f0d1aa50e52
Alex Ho <jkalexho220@gmail.com>
2024-03-21 14:27:30 +0000

    Added Azure Datalake tests

---
+++ src/azure-datalake/test/AzureDataLake_test.go
@@ -0,0 +1,69 @@
+package test
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/microsoft/azure-terraform-cdk-modules/util"
+
+	"github.com/gruntwork-io/terratest/modules/azure"
+	"github.com/gruntwork-io/terratest/modules/random"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// An example of how to test the Terraform module in examples/terraform-azure-example using Terratest.
+func TestAzureDataLake(t *testing.T) {
+	t.Parallel()
+
+	// Location of example file to test
+	example_file := "src/azure-datalake/test/ExampleAzureDataLake.ts"
+
+	// subscriptionID is overridden by the environment variable "ARM_SUBSCRIPTION_ID"
+	subscriptionID := util.GetSubscriptionID()
+	os.Setenv("ARM_SUBSCRIPTION_ID", subscriptionID)
+
+	// Randomize System Name
+	rndName := strings.ToLower(random.UniqueId())
+
+	terraformOptions := &terraform.Options{
+		TerraformBinary: "cdktf",
+		//Terraform Variables
+		Vars: map[string]interface{}{
+			"name": rndName,
+		},
+		TerraformDir: "../../../",
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer func() {
+		util.CdkTFDestroyAll(t, terraformOptions, example_file)
+		os.RemoveAll("./.tempstacks")
+	}()
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	util.CdkTFApplyAllAndIdempotent(t, terraformOptions, example_file)
+
+	// Run `terraform output` to get the values of output variables
+	resourceGroupName := util.CdkTFOutput(t, terraformOptions, "resource_group_name")
+	storageAccountName := util.CdkTFOutput(t, terraformOptions, "storage_account_name")
+	storageAccountTier := util.CdkTFOutput(t, terraformOptions, "storage_account_account_tier")
+	storageAccountKind := util.CdkTFOutput(t, terraformOptions, "storage_account_account_kind")
+
+	// website::tag::4:: Verify storage account properties and ensure it matches the output.
+	storageAccountExists := azure.StorageAccountExists(t, storageAccountName, resourceGroupName, subscriptionID)
+	assert.True(t, storageAccountExists, "storage account does not exist")
+
+	accountKind := azure.GetStorageAccountKind(t, storageAccountName, resourceGroupName, subscriptionID)
+	assert.Equal(t, storageAccountKind, accountKind, "storage account kind mismatch")
+
+	skuTier := azure.GetStorageAccountSkuTier(t, storageAccountName, resourceGroupName, subscriptionID)
+	assert.Equal(t, storageAccountTier, skuTier, "sku tier mismatch")
+
+	actualDNSString := azure.GetStorageDNSString(t, storageAccountName, resourceGroupName, subscriptionID)
+	storageSuffix, _ := azure.GetStorageURISuffixE()
+	expectedDNS := fmt.Sprintf("https://%s.blob.%s/", storageAccountName, storageSuffix)
+	assert.Equal(t, expectedDNS, actualDNSString, "Storage DNS string mismatch")
+}
